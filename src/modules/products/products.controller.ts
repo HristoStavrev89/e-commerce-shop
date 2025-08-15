0507314ff4480843a3fcfefe34887ad63a0c6c70
HStavrev <hristo.stavrev@yahoo.com>
2025-08-15 10:32:43 +0200

    auth guards for creating a products

--- src/modules/products/products.controller.ts
+++ src/modules/products/products.controller.ts
@@ -1,10 +1,16 @@
-import { Body, Controller, Get, Param, ParseIntPipe, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, ParseIntPipe, Post, UseGuards } from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { CreateProductDto } from './dto/create-product.dto';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
+import { RolesGuard } from '../auth/guards/roles.guard';
+import { Roles } from '../auth/decorators/roles.decorator';
+import { Role } from '@prisma/client';
+
 
 @Controller('products')
 export class ProductsController {
     constructor(private readonly productsService: ProductsService) {}
+
 
     @Get()
     findAll() {
@@ -16,6 +22,8 @@ export class ProductsController {
         return this.productsService.findOne(id);
     }
    
+    @UseGuards(JwtAuthGuard, RolesGuard)
+    @Roles(Role.ADMIN)
     @Post()
     create(@Body() createProductDto: CreateProductDto) {
         return this.productsService.create(createProductDto);
